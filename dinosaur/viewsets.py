9f02a4e2d7fd4fba64321881a4ef1f986f4b35a4
LPascholatti <lucaspascholatti@gmail.com>
2019-10-29 15:52:06 +0100

    Kept viewset like previous commit

--- dinosaur/viewsets.py
+++ dinosaur/viewsets.py
@@ -2,7 +2,7 @@ from dinosaur.models import Dinosaur
 from rest_framework import viewsets
 
 # #IMPORT YOUR SERIALIZERS
-from .serializers import *
+from .serializers import DinosaurSerializer
 
 class DinosaurViewSet(viewsets.ModelViewSet):
   serializer_class = DinosaurSerializer
