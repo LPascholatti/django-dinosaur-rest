4d1487aca451c0080be198aa698392537da5253b
LPascholatti <lucaspascholatti@gmail.com>
2019-10-29 09:10:17 +0100

    Create a model Dinosaur

--- dinosaur/models.py
+++ dinosaur/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Dinosaur(models.Dinosaur):
+    TRIASIC = 'TRIASIC'
+    JURASSIC = 'JURASSIC'
+    CRETACEOUS = 'CRETACEOUS'
+    GEOLOGICAL_ERAS = [
+        (TRIASIC, 'Triasic'),
+        (JURASSIC, 'Jurassic'),
+        (CRETACEOUS, 'Cretaceous')
+    ]
+    name: models.CharField(max_length=250)
+    age: models.IntegerField()
+    geological_era: models.CharField(
+      max_length=300,
+      choices=GEOLOGICAL_ERAS
+    )
+    image: models.URLField()
+    region: models.CharField(max_length=100)
