fcc7594c58b279aa2b10c99dfb64edd11670c671
LPascholatti <lucaspascholatti@gmail.com>
2019-10-30 14:50:41 +0100

    Add Dinosaur view

--- dinosaur/views.py
+++ dinosaur/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from dinosaur.models import Dinosaur
+from dinosaur.serializers import DinosaurSerializer
 
-# Create your views here.
+@csrf_exempt
+def dinosaur_list(request):
+  if request.method == 'GET':
+    dinosaurs = Dinosaur.objects.all()
+    serializer = DinosaurSerializer(dinosaurs, many=True)
+    return JsonResponse(serializer.data, safe=False)
