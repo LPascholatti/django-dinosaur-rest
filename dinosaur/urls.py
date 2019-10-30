783bb2ecfa74408f709ce53f62df0ef02105d120
LPascholatti <lucaspascholatti@gmail.com>
2019-10-30 15:08:52 +0100

    Add get dinosaur by pk

--- dinosaur/urls.py
+++ dinosaur/urls.py
@@ -1,7 +1,17 @@
-from rest_framework import routers
-from .viewsets import DinosaurViewSet
+# from rest_framework import routers
+# from .viewsets import DinosaurViewSet
 
-router = routers.DefaultRouter()
-router.register('api/dinosaur', DinosaurViewSet, 'dinosaur')
+# router = routers.DefaultRouter()
+# router.register('api/dinosaur', DinosaurViewSet, 'dinosaur')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+
+#path('api/dinosaur/<int:pk>/', views.dinosaur_detail)
+
+from django.urls import path
+from dinosaur import views
+
+urlpatterns = [
+  path('api/dinosaur/', views.dinosaur_list),
+  path('api/dinosaur/<int:pk>/', views.dinosaur_detail)
+]
--- dinosaur/views.py
+++ dinosaur/views.py
@@ -4,9 +4,35 @@ from rest_framework.parsers import JSONParser
 from dinosaur.models import Dinosaur
 from dinosaur.serializers import DinosaurSerializer
 
+
 @csrf_exempt
 def dinosaur_list(request):
   if request.method == 'GET':
     dinosaurs = Dinosaur.objects.all()
     serializer = DinosaurSerializer(dinosaurs, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+
+@csrf_exempt
+def dinosaur_detail(request, pk):
+    try:
+      dinosaur = Dinosaur.objects.get(pk=pk)
+    except Dinosaur.DoesNotExist:
+      return HttpResponse(status=404)
+
+    if request.method == 'GET':
+      serializer = DinosaurSerializer(dinosaur)
+      return JsonResponse(serializer.data)
+    
+    elif request.method == 'PUT':
+      data = JSONParser().parse(request)
+      serializer = DinosaurSerializer(dinosaur, data=data)
+      if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data)
+      return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+      dinosaur.delete()
+      return HttpResponse(status=204)
+
