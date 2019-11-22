82dd8a2eb6e23c7ba48d9cd8ed307ed0733c3a50
LPascholatti <lucaspascholatti@gmail.com>
2019-11-22 09:47:54 +0100

    Add users endpoints in urls

--- dinosaur/urls.py
+++ dinosaur/urls.py
@@ -15,5 +15,7 @@ from dinosaur import views
 
 urlpatterns = [
   path('api/dinosaur/', views.dinosaur_list),
-  path('api/dinosaur/<int:pk>/', views.dinosaur_detail)
+  path('api/dinosaur/<int:pk>/', views.dinosaur_detail),
+  path('users/', views.UserList.as_view()),
+  path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
